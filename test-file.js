018851f9457ac2e652a42ec085b9c937f2a7c0b9
Tilly Mallabar <tilly.m94@gmail.com>
2024-04-17 16:47:56 +0100

    FIRST TEST

---
+++ test-file.js
@@ -0,0 +1,19 @@
+import { assertEquals } from "./test-framework.js";
+//? Adds Item to Basket
+// Arrange
+let item = "bagel";
+let expected = ["bagel"];
+let actual, result;
+
+// Act
+
+actual = addToBasket(item);
+
+// Assert
+
+result = assertEquals(actual, expected);
+
+// Report
+
+console.log("Adds to basket:");
+console.log(`Test result: ${result}`);
