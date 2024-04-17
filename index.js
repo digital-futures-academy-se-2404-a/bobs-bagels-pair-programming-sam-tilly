c4b866c8d2fe43d96cd2b5c876fbf278935966e9
Sam K <samkoppenhout@live.com>
2024-04-17 18:38:36 +0100

    Basket is now an array of objects, test still fails lol

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-export const addToBasket = (bagel) => {
-    let basket = bagel;
-    console.log(basket);
-    return basket;
+export function Basket(contents) {
+    this.contents = [];
+};
+
+export function Bagel(id) {
+    this.ID = id
+};
+
+export const addToBasket = (toAdd, addTo) => {
+    addTo.contents.push(toAdd);
+    console.log(addTo.contents);
+    return addTo;
 }
--- test-file.js
+++ test-file.js
@@ -1,14 +1,20 @@
 import { assertEquals } from "./test-framework.js";
 import { addToBasket } from "./index.js";
+import { Bagel } from "./index.js";
+import { Basket } from "./index.js";
+
 //? Adds Item to Basket
 // Arrange
-const bagel = {};
-let expected = bagel;
+
+const bagel1 = new Bagel(1);
+const myBasket = new Basket();
+
+let expected = [Bagel];
 let actual, result;
 
 // Act
 
-actual = addToBasket(bagel);
+actual = addToBasket(bagel1, myBasket);
 
 // Assert
 
