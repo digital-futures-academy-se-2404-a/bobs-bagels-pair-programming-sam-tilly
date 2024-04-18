63479b8edfd80a80bd8b861a971abd1e58803310
Sam K <sam@Sams-MacBook-Pro.local>
2024-04-18 17:17:03 +0100

    Added test 2

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -10,6 +10,14 @@ console.log(
     `Add item to basket using addItem and expect that length of basket.contents has increased by 1`
 );
 
+const clearUp = () => {
+    expected = undefined;
+    actual = undefined;
+    result = undefined;
+    testItem = {};
+    basket.basketContents = [];
+}
+
 
 // Arrange
 let expected = basket.basketContents.length + 1;
@@ -26,3 +34,35 @@ result = assertEquals(actual, expected);
 // Report
 console.log(result ? `Pass` : `Fail`);
 !result && console.log(`Expected: ${expected}; Actual: ${actual}`);
+
+//Clear Up
+clearUp();
+console.log(`==================`);
+
+
+//? TEST 2
+//* Verify that the item we tried to add is in the basket
+
+console.log(`Test 2`);
+console.log(`==================`);
+console.log(
+    `Add item to basket using addItem and check if the item added is in the basket`
+);
+
+//Arrange
+expected = true;
+
+//Act
+basket.addItem(testItem);
+actual = basket.basketContents.includes(testItem);
+
+//Assert
+result = assertEquals(actual, expected);
+
+// Report
+console.log(result ? `Pass` : `Fail`);
+!result && console.log(`Expected: ${expected}; Actual: ${actual}`);
+
+//Clear Up
+clearUp();
+console.log(`==================`);
