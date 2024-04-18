b62972108c2bf1f9aebf6e02395b410a9cfc50c1
Sam K <sam@Sams-MacBook-Pro.local>
2024-04-18 17:35:08 +0100

    Adding test 4

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -82,7 +82,35 @@ basket.basketContents.push(originalItem);
 expected = basket.basketContents.length + 1;
 
 // Act
-basket.basketContents.push(testItem);
+basket.addItem(testItem);
+actual = basket.basketContents.length;
+
+// Assert
+result = assertEquals(actual, expected);
+
+// Report
+console.log(result ? `Pass` : `Fail`);
+!result && console.log(`Expected: ${expected}; Actual: ${actual}`);
+
+// Clean Up
+clearUp();
+
+
+//? TEST 4
+//* Verify that an item with null value is not added to the basket
+
+console.log(`Test 4`);
+console.log(`==================`);
+console.log(
+    `Verify that an item with null value is not added to the basket`
+);
+
+// Arrange
+testItem = null;
+expected = basket.basketContents.length;
+
+// Act
+basket.addItem(testItem);
 actual = basket.basketContents.length;
 
 // Assert
